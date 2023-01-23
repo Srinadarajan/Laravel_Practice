7d34cc5d731833ae41bb91a93b118972cea1309c
Kethika <kethikasrinadarajan@gmail.com>
2023-01-23 21:46:31 +0530

    Sample push

--- resources/views/students/create.blade.php
+++ resources/views/students/create.blade.php
@@ -6,7 +6,7 @@
       
       <form action="{{ url('students') }}" method="post">
         {!! csrf_field() !!}
-        <label>Name</label></br>
+        <label>Name :</label></br>
         <input type="text" name="name" id="name" class="form-control"></br>
         <label>Address</label></br>
         <input type="text" name="address" id="address" class="form-control"></br>
